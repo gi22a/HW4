184e0297660b6e1a1c5f76cd60eae17520424855
ilya <arhipov.a2cat@mail.ru>
2023-05-31 12:47:30 +0300

    zadacha29

---
+++ zadacha29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//Обычное, с нулями и прочим
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int InputNumber()
+{
+    Console.Write("Введите размер массива: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+void CreateArray(int[] arr)
+{
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите элемент массива {i}: "); //для наглядности
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    
+}
+void PrintArray(int[] arr){
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+int size = InputNumber();
+int[] array = new int[size];
+CreateArray(array);
+PrintArray(array);
